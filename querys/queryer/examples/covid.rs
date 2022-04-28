318c3cfe615d983b48839d9f3d56ca3c541ca219
matthew.miao <280536928@qq.com>
2022-04-28 15:23:40 +0800

    modify

--- querys/queryer/examples/covid.rs
+++ querys/queryer/examples/covid.rs
@@ -1,31 +1,11 @@
 use anyhow::Result;
-use polars::prelude::*;
-use std::io::Cursor;
 use queryer::query;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
-    let url = "file:///Users/miaojun/code/rust/rust_learn/queryer/csv/co.csv";
+    let url = "file:///Users/miaojun/code/rust/rust_learn/querys/queryer/csv/co.csv";
     // let url = "https://raw.githubusercontent.com/yogjun/rust_learn/main/queryer/csv/co.csv";
-    // let url = "https://raw.githubusercontent.com/yogjun/rust_learn/main/queryer/csv/co.csv";
-    // let data = reqwest::get(url).await?.text().await?;
-    // let df = CsvReader::new(Cursor::new(data))
-    //     .infer_schema(Some(16))
-    //     .finish()?;
-
-    // let filtered = df.filter(&df["new_deaths"].gt(500))?;
-
-    // println!(
-    //     "{:?}",
-    //     filtered.select((
-    //         "location",
-    //         "total_cases",
-    //         "new_cases",
-    //         "total_deaths",
-    //         "new_deaths"
-    //     ))
-    // );
 
     // 使用 sql 从 URL 里获取数据
     let sql = format!( "SELECT location name, total_cases, new_cases, total_deaths, new_deaths  FROM {} where new_deaths >= 500 ORDER BY new_cases DESC", url );
